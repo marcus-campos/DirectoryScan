50f9fc65489870b270610a80fb1ab5d27f121be9
Gabriel Porto Lopes <gabriel_porto_lopes@hotmail.com>
2016-03-10 11:53:33 -0300

    Em construcao

--- DirectoryScan/Form1.Designer.cs
+++ DirectoryScan/Form1.Designer.cs
@@ -28,20 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.butDir1 = new System.Windows.Forms.Button();
+            this.labDir1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labDir2 = new System.Windows.Forms.Label();
+            this.butDir2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // butDir1
+            // 
+            this.butDir1.Location = new System.Drawing.Point(6, 32);
+            this.butDir1.Name = "butDir1";
+            this.butDir1.Size = new System.Drawing.Size(126, 23);
+            this.butDir1.TabIndex = 0;
+            this.butDir1.Text = "Selecione a pasta";
+            this.butDir1.UseVisualStyleBackColor = true;
+            this.butDir1.Click += new System.EventHandler(this.butDir1_Click);
+            // 
+            // labDir1
+            // 
+            this.labDir1.AutoSize = true;
+            this.labDir1.Location = new System.Drawing.Point(6, 16);
+            this.labDir1.Name = "labDir1";
+            this.labDir1.Size = new System.Drawing.Size(22, 13);
+            this.labDir1.TabIndex = 1;
+            this.labDir1.Text = "C:/";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Controls.Add(this.labDir1);
+            this.groupBox1.Controls.Add(this.butDir1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 266);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Diretorio 1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(6, 73);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(121, 97);
+            this.treeView1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labDir2);
+            this.groupBox2.Controls.Add(this.butDir2);
+            this.groupBox2.Location = new System.Drawing.Point(324, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(347, 124);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Diretorio 2";
+            // 
+            // labDir2
+            // 
+            this.labDir2.AutoSize = true;
+            this.labDir2.Location = new System.Drawing.Point(6, 16);
+            this.labDir2.Name = "labDir2";
+            this.labDir2.Size = new System.Drawing.Size(22, 13);
+            this.labDir2.TabIndex = 1;
+            this.labDir2.Text = "C:/";
+            // 
+            // butDir2
+            // 
+            this.butDir2.Location = new System.Drawing.Point(6, 32);
+            this.butDir2.Name = "butDir2";
+            this.butDir2.Size = new System.Drawing.Size(126, 23);
+            this.butDir2.TabIndex = 0;
+            this.butDir2.Text = "Selecione a pasta";
+            this.butDir2.UseVisualStyleBackColor = true;
+            this.butDir2.Click += new System.EventHandler(this.butDir2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(825, 465);
+            this.ClientSize = new System.Drawing.Size(683, 412);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button butDir1;
+        private System.Windows.Forms.Label labDir1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labDir2;
+        private System.Windows.Forms.Button butDir2;
+        private System.Windows.Forms.TreeView treeView1;
+
 
     }
 }
--- DirectoryScan/Form1.cs
+++ DirectoryScan/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -14,6 +15,41 @@ namespace DirectoryScan
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public DirectoryInfo dir1 = null;
+        DirectoryScan dirscan1 = new DirectoryScan();
+
+        private void butDir1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            //instanciar o FolderBrowserDialog
+            folderBrowserDialog.ShowDialog();
+            //abrir o FolderBrowserDialog
+
+            try
+            {
+                dir1 = new DirectoryInfo(folderBrowserDialog.SelectedPath.ToString());
+                //Converter string para DirectoryInfo
+
+                if (dir1.Exists)
+                {
+                    labDir1.Text = dir1.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+
+        }
+
+        private void butDir2_Click(object sender, EventArgs e)
+        {
+
         }
 
     }
