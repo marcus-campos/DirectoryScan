0c80a6145a8d00ac82c66d7b00691b12ef0c38ac
Gabriel Porto Lopes <gabriel_porto_lopes@hotmail.com>
2016-03-10 08:59:05 -0300

    Tela limpa

--- DirectoryScan/Form1.cs
+++ DirectoryScan/Form1.cs
@@ -16,9 +16,5 @@ namespace DirectoryScan
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Teste");
-        }
     }
 }
